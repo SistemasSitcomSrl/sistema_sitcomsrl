d3fc81222f4d9fa012a7ec1f4e83fd71b635239f
SistemasSitcomSrl <sitcomsrl.sistemas@gmail.com>
2024-05-14 11:48:27 -0400

    Production32 14/05/2024

--- app/Livewire/movimientos/ViewMovements.php
+++ app/Livewire/movimientos/ViewMovements.php
@@ -292,6 +292,7 @@ class ViewMovements extends Component
             ->where('movements.id_project', $this->receipt_number)
             ->where('movements.state', $this->state_receipt)
             ->where('name_equipment', 'like', '%' . $this->search . '%')  
+            ->orderBy('movements.id', 'desc')
             ->paginate($this->cant);
 
         $movements_histories = MovementHistory::select()->get();
